863a9a5d96f8a70126bebb3e52ea7bb6d85d1ab0
5tien <Sten.bosma00@gmail.com>
2022-02-11 13:04:36 +0100

    Camera n shit

--- Assets/Scripts/Camera/CameraMover.cs
+++ Assets/Scripts/Camera/CameraMover.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
+    [SerializeField] private float CameraSpeed = 1;
+
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 position;
     [SerializeField] private Vector3 offset;
+
+    private void Update()
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+            moveDirection += new Vector3(1, 0, 1);
+        if (Input.GetKey(KeyCode.A))
+            moveDirection += new Vector3(-1, 0, 1);
+        if (Input.GetKey(KeyCode.S))
+            moveDirection += new Vector3(-1, 0, -1);
+        if (Input.GetKey(KeyCode.D))
+            moveDirection += new Vector3(1, 0, -1);
+
+        moveDirection = moveDirection * Time.deltaTime * CameraSpeed;
+
+        position += moveDirection;
+    }
 
     private void FixedUpdate()
     {
         Vector3 pos = target != null ? target.position : position;
 
-        pos = pos + offset; new Vector3(-45, 67.5f, -45);
+        pos += offset;
 
-        this.transform.position = pos;
+        this.transform.position = Vector3.Lerp(this.transform.position, pos, Time.deltaTime * CameraSpeed);
     }
 }
