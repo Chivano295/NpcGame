b24befa872727f620f971a679794c12fc3bc5ed3
Chivano295 <crijsdam@hotmail.com>
2022-02-18 16:47:27 +0100

    dggwg

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,23 +6,28 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     
+    //gameobject for the credits list and main menu list so you can switch between them
     public GameObject Mainmenu;
     public GameObject Credits;
+    //changes the scene based on string
     public void ChangeScene(string scene)
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(scene);
     }
+    //quits the game
     public void Quitgame()
     {
         Application.Quit();
     }
 
+    //turns on the credits
     public void CreditsOn()
     {
         Mainmenu.SetActive(false);
         Credits.SetActive(true);
     }
+    //turns credits off
     public void CreditsOff()
     {
         Mainmenu.SetActive(true);
